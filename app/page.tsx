5990501775948e6aeba3b7c5fe8133d3b7adff05
NMPhap <21522441@gm.uit.edu.vn>
2023-06-07 21:40:04 +0700

    <Fix>: Build crash

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,6 @@ export default function Home() {
   const url = document.URL + "/landing";
   return (
     <>
-      <header>
-        <meta http-equiv="Refresh" content={`0; url='${url}'`} />
-      </header>
-      <main></main>
     </>
   );
 }
