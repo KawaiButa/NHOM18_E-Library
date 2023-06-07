e9679c566e441b72a7e2614ea3a66c5c41ecafd0
NMPhap <21522441@gm.uit.edu.vn>
2023-06-07 21:27:01 +0700

    <Fix>: Build crash

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 import React from "react";
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Card, Button, Container, Row, Col } from "react-bootstrap";
-import BookTag from "./components/bookTag/bookTag";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 
 export default function Home() {
