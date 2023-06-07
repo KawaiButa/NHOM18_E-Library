51173f2875bf761f73df46be0ffdcc030387ab51
NMPhap <21522441@gm.uit.edu.vn>
2023-06-07 21:43:04 +0700

    <Fix>: Build crash

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
-import React from "react";
-
+import React, { useEffect } from "react";
 export default function Home() {
-  const url = document.URL + "/landing";
+  url: String;
+  useEffect(() => { this.url = document.URL + "/landing";
+},[])
   return (
     <>
+      <header>
+        <meta http-equiv="Refresh" content={`0; url='${this.url}'`} />
+      </header>
+      <main></main>
     </>
   );
 }
