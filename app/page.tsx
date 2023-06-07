df21ba5f8fabfb1abd2e10422abe3d24156538d7
NMPhap <21522441@gm.uit.edu.vn>
2023-06-07 21:37:41 +0700

    <Fix>: Build crash

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 import React from "react";
 
-
 export default function Home() {
- // const url = document.URL + "/landing";
-  return <></>;
-
+  const url = document.URL + "/landing";
+  return (
+    <>
+      <header>
+        <meta http-equiv="Refresh" content={`0; url='${url}'`} />
+      </header>
+      <main></main>
+    </>
+  );
 }
