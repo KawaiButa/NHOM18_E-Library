fe9d690d0885d2780946f67fa29703e42b4c7cdc
NMPhap <21522441@gm.uit.edu.vn>
2023-06-07 21:32:43 +0700

    <Fix>: build crash

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,6 @@ import React from "react";
 
 export default function Home() {
   const url = document.URL + "/landing";
-  return <meta http-equiv="Refresh" content={`0; url='${url}'`} />
+  return <><meta http-equiv="Refresh" content={`0; url='${url}'`} /></>;
 
 }
