d57e062ed82a5452eb7266ab867fe47d671dbeff
Ly Thanh Tu Anh <tuanhlythanh@gmail.com>
2023-06-07 15:11:16 +0700

    Add BorrowForm

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import BookTag from "./bookTag";
-import MemberForm from "./components/memberForm"
+import MemberForm from "./components/memberForm";
+import BorrowForm from "./components/borrowForm";
 export default function Home() {
-  return (
-    <main className="d-flex justify-content-center align-items-center">
-      <MemberForm/>
-    </main>
-  );
+    return (
+        <main className="d-flex justify-content-center align-items-center">
+            <MemberForm />
+        </main>
+    );
 }
