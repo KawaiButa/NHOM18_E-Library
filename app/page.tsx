1bb172a77cc51db4d35c9026b4f48bd8008346eb
Ly Thanh Tu Anh <tuanhlythanh@gmail.com>
2023-06-09 18:50:53 +0700

    Detail Borrow Card Component

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,11 @@ import MemberForm from "./components/memberForm";
 import BorrowForm from "./components/borrowForm";
 import MemberListCard from "./memberListComponent";
 import BorrowListCard from "./components/borrowListCard";
+import BorrowCardDetail from "./components/borrowCardDetail";
 export default function Home() {
     return (
         <main className="d-flex justify-content-center align-items-center">
-            <BorrowForm />
+            <BorrowCardDetail />
         </main>
     );
 }
