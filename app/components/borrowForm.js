08c942f1c470b8a352ba1ad38a89354eab7fccba
Ly Thanh Tu Anh <tuanhlythanh@gmail.com>
2023-06-14 13:16:25 +0700

    Add counter

--- app/components/borrowForm.js
+++ app/components/borrowForm.js
@@ -1,16 +1,21 @@
+"use client";
 import {
     Button,
     Card,
     Container,
     Form,
     Row,
+    Image,
     Col,
     FormGroup,
     Stack,
 } from "react-bootstrap";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Montserrat } from "next/font/google";
+import useBook from "../../lib/useBook";
+import useUser from "../../lib/useUser";
+import styles from "./borrowForm.module.css";
 
 const montserrat = Montserrat({
     weight: ["400", "700"],
@@ -18,6 +23,23 @@ const montserrat = Montserrat({
 });
 
 export default function BorrowForm() {
+    const { user } = useUser();
+    const { books } = useBook();
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        var list = document.getElementById("book");
+        list.replaceChildren([]);
+        books?.forEach((element) => {
+            var option = document.createElement("option");
+            option.value = element.name;
+            option.id = element.id;
+            list.appendChild(option);
+        });
+        var memberName = document.getElementById("memberName");
+        memberName.value = user?.name;
+    }, [books, user]);
     return (
         <>
             <Row
@@ -79,31 +101,81 @@ export default function BorrowForm() {
                                     >
                                         Member Name
                                     </Form.Label>
-                                    <Form.Control as="select" size="lg">
-                                        <option>Nguyễn Minh Pháp</option>
-                                        <option>Cao Quảng An Hưng</option>
-                                        <option>Phạm Nguyễn Nhật Duy</option>
-                                        <option>Lý Thanh Tú Anh</option>
-                                    </Form.Control>
+                                    <input
+                                        id="memberName"
+                                        type="text"
+                                        list="name"
+                                        className="form-control"
+                                        disabled={
+                                            user?.role == "admin" ? false : true
+                                        }
+                                    />
+                                    <datalist id="name"></datalist>
                                 </Form.Group>
                             </Col>
 
                             <Stack direction="horizontal" gap={5}>
                                 <Col md={8}>
-                                    <Form.Group>
-                                        <Form.Label
-                                            className={montserrat.className}
+                                    <Stack direction="horizontal">
+                                        <Form.Group>
+                                            <Form.Label
+                                                className={montserrat.className}
+                                            >
+                                                Book Name
+                                            </Form.Label>
+                                            <Form.Control
+                                                as="input"
+                                                className="shadow-none"
+                                                list="book"
+                                                style={{ width: "475px" }}
+                                            ></Form.Control>
+                                            <datalist id="book"></datalist>
+                                        </Form.Group>
+                                        <Stack
+                                            direction="horizontal"
+                                            style={{
+                                                marginTop: "32px",
+                                                marginLeft: "0px",
+                                            }}
                                         >
-                                            Book Name
-                                        </Form.Label>
-                                        <Form.Control as="select" size="lg">
-                                            <option>1</option>
-                                            <option>2</option>
-                                            <option>3</option>
-                                            <option>4</option>
-                                            <option>5</option>
-                                        </Form.Control>
-                                    </Form.Group>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="100"
+                                                readOnly
+                                                className="shadow-none"
+                                                style={{
+                                                    textAlign: "left",
+                                                    width: "80px",
+                                                }}
+                                            />
+                                            <Stack
+                                                direction="vertical"
+                                                style={{
+                                                    marginLeft: "-30px",
+                                                    marginTop: "5px",
+                                                }}
+                                            >
+                                                <Image
+                                                    src="/up.png"
+                                                    style={{
+                                                        width: "20px",
+                                                        top: "1px",
+                                                        position: "relative",
+                                                        height: "20px",
+                                                    }}
+                                                ></Image>
+                                                <Image
+                                                    src="/down.png"
+                                                    style={{
+                                                        width: "20px",
+                                                        bottom: "2px",
+                                                        position: "relative",
+                                                        height: "20px",
+                                                    }}
+                                                ></Image>
+                                            </Stack>
+                                        </Stack>
+                                    </Stack>
                                 </Col>
 
                                 <Button
@@ -113,6 +185,7 @@ export default function BorrowForm() {
                                     style={{
                                         width: "130px",
                                         top: "15px",
+                                        left: "-12px",
                                         position: "relative",
                                         fontWeight: "700",
                                         background: "#44B8CB",
@@ -131,11 +204,25 @@ export default function BorrowForm() {
                                         >
                                             Book order list
                                         </Form.Label>
-                                        <Form.Control
-                                            size="lg"
-                                            as="textarea"
-                                            rows={10}
-                                        />
+                                        <table
+                                            className={`${styles.tableFixed} table`}
+                                        >
+                                            <thead>
+                                                <tr>
+                                                    <th class="col-xs-3">
+                                                        STT
+                                                    </th>
+                                                    <th class="col-xs-3">
+                                                        Name
+                                                    </th>
+                                                    <th class="col-xs-6">
+                                                        {" "}
+                                                        Amount
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody></tbody>
+                                        </table>
                                     </Form.Group>
                                 </Col>
                                 <Button
@@ -161,13 +248,16 @@ export default function BorrowForm() {
                                     className={montserrat.className}
                                     style={{
                                         width: "231px",
-                                        height: "88px",
+                                        height: "76px",
                                         borderRadius: "20px",
+                                        position: "absolute",
                                         backgroundColor: "#44B8CB",
                                         color: "white",
                                         fontWeight: "bold",
                                         fontSize: "35px",
                                         borderColor: "#44B8CB",
+                                        padding: "0px",
+                                        bottom: "-250px",
                                     }}
                                 >
                                     Summit
