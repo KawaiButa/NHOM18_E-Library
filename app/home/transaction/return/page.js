c615f6a928572370e552bb25964192951c32e77e
NMPhap <21522441@gm.uit.edu.vn>
2023-06-22 16:00:38 +0700

    <Fix> Fix 1.12

--- app/home/transaction/return/page.js
+++ app/home/transaction/return/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReturnListCard from "../../../../components/returnListCard/returnListCard";
 import axios from "axios";
 import useProfile from "../../../../lib/useProfile";
