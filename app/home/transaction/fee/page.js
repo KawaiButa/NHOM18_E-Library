d3b6ca24473d91940c612c12039e1628436292cf
NMPhap <21522441@gm.uit.edu.vn>
2023-06-25 14:04:28 +0700

    <Fix>: Patch 1.43

---
+++ app/home/transaction/fee/page.js
@@ -0,0 +1,59 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import FeeListCard from "../../../../components/remindCard/feeListCard";
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+import useProfile from "../../../../lib/useProfile";
+export default function Transaction() {
+    const [member, setMember] = useState();
+    const { profile } = useProfile();
+    const search = useSearchParams();
+    useEffect(() => {
+      async function onCreate() {
+        if (profile) {
+          console.log(profile.id);
+          await axios
+            .get("/api/profile/" + profile?.id, {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            })
+            .then((response) => {
+              if (response.status == 200) setMember(response.data);
+              if (response.status == 204) setMember(null);
+            });
+        }
+      }
+      onCreate();
+    }, [profile]);
+    if (profile)
+      if (profile.role == "admin" || search.get("borrower"))
+        return (
+          <div>
+            <FeeListCard />
+          </div>
+        );
+      else if (!member)
+        return (
+          <>
+            <h2>{"You don't have a reader card connect to this account."}</h2>
+            <h2>
+              {"Please contact to the library admin to create a reader card"}
+            </h2>
+          </>
+        );
+      else {
+        var endpoint = "/home/transaction/return?borrower=" + member.readerId;
+        return (
+          <>
+            <header>
+              <meta http-equiv="Refresh" content={`0; url='` + endpoint + `'`} />
+            </header>
+            <main></main>
+          </>
+        );
+      }
+      else return <></>
+  }
+  
--- app/home/transaction/fee/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-import FeeListCard from "../../../../components/remindCard/feeListCard";
-export default function Transaction(){
-    return <div >
-        <FeeListCard></FeeListCard>
-    </div>
-}
